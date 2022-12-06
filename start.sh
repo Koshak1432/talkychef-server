ce0b02013d9c273408c3ae28e3ff86a63861d329
Diif <30970636+Diif@users.noreply.github.com>
2022-12-06 13:21:18 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export DATASOURCE_URL=jdbc:postgresql://localhost:5432/postgres
-export DATASOURCE_USERNAME=postgres
-export DATASOURCE_PASSWORD=postgres
-export SCHEMA_NAME=public
-
-./gradlew bootRun
