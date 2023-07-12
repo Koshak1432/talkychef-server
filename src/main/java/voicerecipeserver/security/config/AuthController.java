0343df443649c95176ae9c97e2674aef7642760f
Пашкова Алина Сергеевна <al.passshkova@gmail.com>
2023-07-12 16:10:10 +0700

    Changed authController

--- src/main/java/voicerecipeserver/security/config/AuthController.java
+++ src/main/java/voicerecipeserver/security/config/AuthController.java
@@ -27,13 +27,13 @@ public class AuthController {
         return ResponseEntity.ok(token);
     }
 
-    @PostMapping("/token")
+    @PostMapping("/auth/token")
     public ResponseEntity<JwtResponse> getNewAccessToken(@RequestBody RefreshJwtRequest request) throws AuthException {
         final JwtResponse token = authService.getAccessToken(request.getRefreshToken());
         return ResponseEntity.ok(token);
     }
 
-    @PostMapping("/refresh")
+    @PostMapping("/auth/refresh")
     public ResponseEntity<JwtResponse> getNewRefreshToken(@RequestBody RefreshJwtRequest request) throws AuthException {
         final JwtResponse token = authService.refresh(request.getRefreshToken());
         return ResponseEntity.ok(token);
